b68bc04c73f6968506409191672167bb480b88ec
Dave <drregg6@gmail.com>
2017-06-23 15:51:34 -0400

    added a transition and comments

--- assets/js/moods.js
+++ assets/js/moods.js
@@ -1,3 +1,17 @@
+/*
+
+array of moods and colors associated with moods
+have a set amount of color options
+if the mood is not found, return white
+
+
+:: idea when using mongodb ::
+maybe have a button to click
+    if the mood is not found
+        update the db with that feeling
+
+*/
+
 var moodsArray = [
     {
         moods: ['happy', 'joy', 'joyous', 'glad', 'great', 'excellent', 'fabulous'],
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,23 @@
-var input = document.getElementById('input');
-var container = document.getElementsByClassName('container')[0];
+var input, container, val, randomNum;
+
+input = document.getElementById('input');
+container = document.getElementsByClassName('container')[0];
 
 console.log(input);
 
 
 // this will not reset when any letter is deleted
 window.onkeyup = function(ev) {
-    var val = ev.target.value.toLowerCase();
+    val = ev.target.value.toLowerCase();
     console.log(val);
     
     moodsArray.forEach(function(object) {
         object.moods.forEach(function(mood) {
             if (val === mood) {
+                randomNum = Math.floor(Math.random() * object.colors.length);
+                
                 container.style.backgroundColor = object.colors[randomNum];
             }
         });
     });
 }
-
-var randomNum = Math.floor(Math.random() * moodsArray[0].colors.length);
