93cbf148befbe8bf856cd3a73388e5c605be4eff
Dave <drregg6@gmail.com>
2017-06-23 17:07:26 -0400

    moods.js has synonyms updated

--- assets/js/moods.js
+++ assets/js/moods.js
@@ -9,16 +9,66 @@ if the mood is not found, return white
 maybe have a button to click
     if the mood is not found
         update the db with that feeling
+        
+maybe find a list of moods
+or positive / negative connotated words
 
 */
 
 var moodsArray = [
     {
-        moods: ['happy', 'joy', 'joyous', 'glad', 'great', 'excellent', 'fabulous'],
-        colors: ['yellow', 'orange', 'pink']
+        /* happy */
+        moods: ['happy', 'cheerful', 'peaceful', 'delighted', 'ecstatic', 'elated', 'lively', 'jubilant', 'merry', 'thrilled', 'optimistic', 'upbeat', 'joyous', 'joyful', 'glad', 'gay', 'content', 'blissful'],
+        colors: ['yellow']
     },
     {
-        moods: ['sad', 'depressed', 'crying', 'unjoyful'],
-        colors: ['blue', 'aqua', 'purple']
+        /* sad */
+        moods: ['sad', 'bitter', 'dismal', 'heartbroken', 'pessimistic', 'mournful', 'somber', 'sorrowful', 'distressed', 'depressed', 'blue', 'down', 'glum', 'gloomy', 'hurting', 'hurt', 'morbid', 'low', 'downcast', 'troubled', 'crappy', 'poopy'],
+        colors: ['dark blue']
+    },
+    {
+        /* neutral */
+        moods: ['neutral', 'disinterested', 'inactive', 'indifferent', 'bored', 'undecided', 'calm', 'cool', 'nonchalant', 'aloof', 'bystanding', 'detached', 'easy'],
+        colors: ['grey']
+    },
+    {
+        /* mad */
+        moods: ['mad', 'absurd', 'angry', 'delirious', 'demented', 'frantic', 'frenzied', 'psychotic', 'pissed', 'crazed', 'mental', 'rabid', 'unhinged', 'unreasonable', 'unstable'],
+        colors: ['red']
+    },
+    {
+        /* silly */
+        moods: ['silly', 'childish', 'crazy', 'idiotic', 'ridiculous', 'simple', 'stupid', 'asinine', 'brainless', 'braindead', 'dizzy', 'dippy', 'ignorant', 'immature', 'nitwitted', 'senseless', 'unwise', 'witless', 'unintelligent', 'simpleminded'],
+        colors: ['orange']
+    },
+    {
+        /* mysterious */
+        moods: ['dark', 'curious', 'strange', 'unknown', 'weird', 'mystifying', 'perplexing', 'enigmatic', 'enigma', 'cryptic', 'perplexing', 'obscure', 'secretive', 'difficult', 'covert', 'arcane', 'hidden'],
+        colors: ['dark shadowy colors']
+    },
+    {
+        /* powerful */
+        moods: ['authoritative', 'impressive', 'mighty', 'persuasive', 'influential', 'impressive', 'forceful', 'energetic', 'dynamic', 'dominate', 'dominant', 'vigorous', 'potent', 'competent', 'controlling', 'supreme'],
+        colors: ['purple']
+    },
+    {
+        /* confused */
+        moods: ['confused', 'bewildered', 'disorganized', 'baffled', 'befuddled', 'dazed', 'distracted', 'muddled', 'perplexed', 'puzzled', 'flummoxed', 'stumped', 'thrown', 'screwy', 'screwed'],
+        colors: ['light blue']
+    },
+    {
+        /* scared */
+        moods: ['scared', 'shy', 'afraid', 'anxious', 'fearful', 'panicked', 'panicky', 'petrified', 'shaken', 'terrified', 'aghast', 'startled', 'apprehensive', 'bashful', 'cautious', 'hesitant', 'humble', 'introverted', 'nervous', 'reluctant', 'sheepish', 'timid'],
+        colors: ['light grey and whites']
+    },
+    {
+        /* jealous */
+        moods: ['jealous', 'envious', 'possessive', 'protective', 'resentful', 'skeptical', 'suspicious', 'envying', 'guarded', 'watchful', 'zealous'],
+        colors: ['green']
+    },
+    {
+        /* sexy */
+        moods: ['sexy', 'hot', 'cute', 'provocative', 'inviting', 'racy', 'seductive', 'sensual', 'sensuous', 'arousing', 'cuddly', 'flirty', 'flirtatious', 'flirt', 'risque', 'steamy', 'titillating', 'voluptuous'],
+        colors: ['pink']
     }
 ];
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,29 @@
-var input, container, val, randomNum;
+var input, container, val, randomNum, body;
 
 input = document.getElementById('input');
 container = document.getElementsByClassName('container')[0];
-
+body = document.getElementsByTagName('body')[0];
+// test
 console.log(input);
 
 
-// this will not reset when any letter is deleted
+// this will not reset when any letters are deleted
+// read why this is
 window.onkeyup = function(ev) {
+    // read about ev.target
     val = ev.target.value.toLowerCase();
     console.log(val);
     
+    // for each object in the moodsArray
     moodsArray.forEach(function(object) {
+        // for each mood in object.moods
         object.moods.forEach(function(mood) {
+            // if there's a match, update the color of the background
             if (val === mood) {
                 randomNum = Math.floor(Math.random() * object.colors.length);
                 
-                container.style.backgroundColor = object.colors[randomNum];
+                // to a random color from the colors array in that same object
+                body.style.backgroundColor = object.colors[randomNum];
             }
         });
     });
