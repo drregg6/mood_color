4d2a92205a6e1b1784966616107ca97d2d3fef60
Dave <drregg6@gmail.com>
2017-06-23 21:48:58 -0400

    complete preliminary version

--- assets/js/moods.js
+++ assets/js/moods.js
@@ -36,48 +36,56 @@ var moodsArray = [
     },
     {
         /* mad */
-        moods: ['mad', 'absurd', 'angry', 'delirious', 'demented', 'frantic', 'frenzied', 'psychotic', 'pissed', 'crazed', 'mental', 'rabid', 'unhinged', 'unreasonable', 'unstable'],
+        moods: ['mad', 'absurd', 'angry', 'delirious', 'demented', 'frantic', 'frenzied', 'psychotic', 'pissed', 'crazed', 'mental', 'rabid', 'unhinged', 'unreasonable', 'unstable', 'upset'],
         colors: ['#f00', '#e00000', '#b80000', '#f81200', '#ff0013', '#ff1300', '#b30000', '#c00'],
         fontColor: '#fff'
     },
     {
         /* silly */
         moods: ['silly', 'childish', 'crazy', 'idiotic', 'ridiculous', 'simple', 'stupid', 'asinine', 'brainless', 'braindead', 'dizzy', 'dippy', 'ignorant', 'immature', 'nitwitted', 'senseless', 'unwise', 'witless', 'unintelligent', 'simpleminded'],
-        colors: ['orange']
+        colors: ['#ffa500', '#e09200', '#b87700', '#ffb833', '#f2a20d', '#e69e19', '#c8b800', '#ff6c00', '#fc0', '#ffdf00'],
+        fontColor: '#000'
     },
     {
         /* mysterious */
         moods: ['dark', 'curious', 'strange', 'unknown', 'weird', 'mystifying', 'perplexing', 'enigmatic', 'enigma', 'cryptic', 'perplexing', 'obscure', 'secretive', 'difficult', 'covert', 'arcane', 'hidden'],
-        colors: ['dark shadowy colors']
+        colors: ['#092e35', '#053b02', '#0d214a', '#190047', '#802600', '#4e0052', '#85004b'],
+        fontColor: '#fff'
     },
     {
         /* powerful */
-        moods: ['authoritative', 'impressive', 'mighty', 'persuasive', 'influential', 'impressive', 'forceful', 'energetic', 'dynamic', 'dominate', 'dominant', 'vigorous', 'potent', 'competent', 'controlling', 'supreme'],
-        colors: ['purple']
+        moods: ['powerful', 'authoritative', 'impressive', 'mighty', 'persuasive', 'influential', 'impressive', 'forceful', 'energetic', 'dynamic', 'dominate', 'dominant', 'vigorous', 'potent', 'competent', 'controlling', 'supreme'],
+        colors: ['#903fe0', '#8a1986', '#671365', '#8f09f6', '#6d07bb', '#590699', '#9618f7', '#6b00bd', '#5a009e'],
+        fontColor: '#fff'
     },
     {
         /* confused */
         moods: ['confused', 'bewildered', 'disorganized', 'baffled', 'befuddled', 'dazed', 'distracted', 'muddled', 'perplexed', 'puzzled', 'flummoxed', 'stumped', 'thrown', 'screwy', 'screwed'],
-        colors: ['light blue']
+        colors: ['#00bbff', '#33c9ff', '#5cd3ff', '#0db5f2', '#0082ff', '#00a8ff', '#0bf', '#2192ff'],
+        fontColor: '#000'
     },
     {
         /* scared */
         moods: ['scared', 'shy', 'afraid', 'anxious', 'fearful', 'panicked', 'panicky', 'petrified', 'shaken', 'terrified', 'aghast', 'startled', 'apprehensive', 'bashful', 'cautious', 'hesitant', 'humble', 'introverted', 'nervous', 'reluctant', 'sheepish', 'timid'],
-        colors: ['light grey and whites']
+        colors: ['#dedede', '#f5f5f5', '#c2c2c2', '#999999', '#adadad'],
+        fontColor: '#000'
     },
     {
         /* jealous */
         moods: ['jealous', 'envious', 'possessive', 'protective', 'resentful', 'skeptical', 'suspicious', 'envying', 'guarded', 'watchful', 'zealous'],
-        colors: ['green']
+        colors: ['#0f0', '#47ff47', '#70ff70', '#19e619', '#26d926', '#0c0', '#00a300', '#007a00', '#005200'],
+        fontColor: '#000'
     },
     {
         /* sexy */
         moods: ['sexy', 'hot', 'cute', 'provocative', 'inviting', 'racy', 'seductive', 'sensual', 'sensuous', 'arousing', 'cuddly', 'flirty', 'flirtatious', 'flirt', 'risque', 'steamy', 'titillating', 'voluptuous', 'erotic'],
-        colors: ['pink']
+        colors: ['#ff0091', '#ff33a7', '#ff5cb8', '#ff0091', '#ff1c7c', '#ff007d', '#ff006a', '#ff00ca'],
+        fontColor: '#000'
     },
     {
         /* unattractive */
         moods: ['unattractive', 'disgusting', 'repugnant', 'repulsive', 'unappealing', 'bad-looking', 'beastly', 'deformed', 'disfigured', 'gross', 'grotesque', 'hideous', 'horrid', 'monstrous', 'unalluring'],
-        colors: ['browns']
+        colors: ['#310d0d', '#30120d', '#30150d', '#370606', '#3d0000', '#401111'],
+        fontColor: '#fff'
     }
 ];
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@ add a button for onclick effect, keyup isn't effective enough
 
 */
 
-var input, container, val, randomNum, body;
+var input, container, val, randomNum, body, flag, allColors;
 
 input = document.getElementById('input');
 container = document.getElementsByClassName('container')[0];
@@ -15,23 +15,37 @@ console.log(input);
 
 // this will not reset when any letters are deleted
 // read why this is
+
+// you can separate a lot of this code into different funcs
 window.onkeyup = function(ev) {
     // read about ev.target
     val = ev.target.value.toLowerCase();
+    flag = false; // the flag waves if found
     console.log(val);
     
     // for each object in the moodsArray
     moodsArray.forEach(function(object) {
         // for each mood in object.moods
-        object.moods.forEach(function(mood) {
-            // if there's a match, update the color of the background
-            if (val === mood) {
-                randomNum = Math.floor(Math.random() * object.colors.length);
-                
-                // to a random color from the colors array in that same object
-                body.style.backgroundColor = object.colors[randomNum];
-                input.style.color = object.fontColor;
-            }
-        });
+        
+        // if there's a match, update the color of the background
+        if (object.moods.indexOf(val) !== -1) {
+            flag = true;
+            console.log('found!');
+            
+            // to a random color from the colors array in that same object
+            randomNum = Math.floor(Math.random() * object.colors.length);
+            body.style.backgroundColor = object.colors[randomNum];
+            input.style.color = object.fontColor;
+        }
     });
+    if (flag === false) {
+        
+        // there should be an easier way to do this
+        allColors = moodsArray[0].colors.concat(moodsArray[1].colors, moodsArray[2].colors, moodsArray[3].colors, moodsArray[4].colors, moodsArray[5].colors, moodsArray[6].colors, moodsArray[7].colors, moodsArray[8].colors, moodsArray[9].colors, moodsArray[10].colors, moodsArray[11].colors)
+        
+        randomNum = Math.floor(Math.random() * allColors.length);
+        
+        body.style.backgroundColor = allColors[randomNum];
+        input.style.color = 'rgba(0,0,0,0.5)';
+    }
 }
